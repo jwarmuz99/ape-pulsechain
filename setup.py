4f7e0050a77fcbf20d507a704eb7a9169d87915d
Jakub Warmuz <jwarmuz99@gmail.com>
2024-03-25 10:05:00 +0100

    feat: pulsechain plugin

--- setup.py
+++ setup.py
@@ -50,25 +50,28 @@ with open("./README.md") as readme:
 
 
 setup(
-    name="<PYPI_NAME>",
+    name="ape-pulsechain",
     use_scm_version=True,
     setup_requires=["setuptools_scm"],
-    description="""<PYPI_NAME>: <SHORT_DESCRIPTION>""",
+    description="""ape-pulsechain: Ape Ecosystem Plugin for PulseChain""",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="ApeWorX Ltd.",
     author_email="admin@apeworx.io",
-    url="https://github.com/ApeWorX/<REPO_NAME>",
+    url="https://github.com/ApeWorX/ape-pulsechain",
     include_package_data=True,
-    install_requires=[],
+    install_requires=[
+        "eth-ape>=0.7.6,<0.8",
+        "ethpm-types",  # Use same version as eth-ape
+    ],
     python_requires=">=3.8,<4",
     extras_require=extras_require,
-    py_modules=["<MODULE_NAME>"],
+    py_modules=["ape_pulsechain"],
     license="Apache-2.0",
     zip_safe=False,
     keywords="ethereum",
     packages=find_packages(exclude=["tests", "tests.*"]),
-    package_data={"<MODULE_NAME>": ["py.typed"]},
+    package_data={"ape_pulsechain": ["py.typed"]},
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
